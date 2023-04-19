61d8e5e972ab73a51c497132917fa12663939322
Antoine FUSILIER <antoinefusilier@gmail.com>
2023-04-19 14:27:42 +0100

    ...

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -2,14 +2,30 @@ import { Injectable } from '@angular/core';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { environment as ENV } from 'src/environments/environment';
+import { resourceUsage } from 'process';
 
+
+const app = initializeApp(ENV.firebaseConfig);
+const analytics = getAnalytics(app);
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
   constructor() {
-    const app = initializeApp(ENV.firebaseConfig);
-    const analytics = getAnalytics(app);
+
   }
+
+  getObject = async (obj: 'app' | 'analytics' = 'app') => {
+
+    if(obj === 'app'){
+      return app;
+    } else if (obj === 'analytics'){
+      return analytics;
+    } else {
+      return app;
+    }
+
+  }
+
 }
